8db34cb81c47121ec256c27c0bd07a893a54fa5e
magira01 <magira01@hotmail.com>
2021-11-05 16:18:49 -0300

    first commit

---
+++ src/main/java/ar/com/ada/api/apiempleadas/ApiempleadasApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.apiempleadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiempleadasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiempleadasApplication.class, args);
+	}
+
+}
